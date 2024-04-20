a78185aea967760d8fca991a3a66c83714f89484
Nuxea <lescieux.am@gmail.com>
2024-04-20 17:18:45 +0200

    Creating JSON API Endpoints, Creating the new Route & Controller, Returning JSON, Adding a Model Class, Creating the Model Objects, Installation Symfony Serializer

---
+++ src/Controller/StarshipApiController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\Starship;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class StarshipApiController extends AbstractController
+{
+    #[Route('/api/starships', name: 'starships')]
+    public function getCollection(): Response
+    {
+        $starships = [
+            new Starship(
+                1,
+                'USS LeafyCruiser (NCC-0001)',
+                'Garden',
+                'Jean-Luc Pickles',
+                'taken over by Q'
+            ),
+            new Starship(
+                2,
+                'USS Espresso (NCC-1234-C)',
+                'Latte',
+                'James T. Quick!',
+                'repaired',
+            ),
+            new Starship(
+                3,
+                'USS Wanderlust (NCC-2024-W)',
+                'Delta Tourist',
+                'Kathryn Journeyway',
+                'under construction',
+            ),
+        ];
+
+        return $this->json($starships);
+    }
+}
