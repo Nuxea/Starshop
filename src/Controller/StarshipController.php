9aea38bece781e210a5f5cd22ea1a9252b4a0625
Nuxea <lescieux.am@gmail.com>
2024-04-21 11:48:42 +0200

    Creating the Show Page, Creating the Template, Linking Between Pages,

---
+++ src/Controller/StarshipController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\StarshipRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class StarshipController extends AbstractController
+{
+    #[Route('/starships/{id<\d+>}', name: 'app_starship_show', methods: ['GET'])]
+    public function show(int $id, StarshipRepository $repository): Response
+    {
+        $ship = $repository->findById($id);
+
+        if (!$ship) {
+            throw $this->createNotFoundException('Starship not found');
+        }
+
+        return $this->render('starship/show.html.twig', [
+            'ship' => $ship,
+        ]);
+    }
+}
