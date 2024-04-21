31bcb56406ae8efe465ac233c222a8dac0a4a139
Nuxea <lescieux.am@gmail.com>
2024-04-21 14:39:17 +0200

    Stimulus: Writing Pro JavaScript

--- importmap.php
+++ importmap.php
@@ -16,4 +16,10 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
+    '@symfony/stimulus-bundle' => [
+        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+    ],
 ];
